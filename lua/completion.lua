073c5853531158fdbf773eec4b10e00c4e38eeb0
Jeremy Quesada <jeremy@Jeremys-Laptop.local>
2024-06-17 00:16:34 -0700

    feat: adding cr cmp mapping

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,5 +1,8 @@
 local cmp = require("cmp")
 
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.shortmess:append("c")
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -14,7 +17,7 @@ cmp.setup({
 	mapping = {
 		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<C-y>"] = cmp.mapping(
+		["<CR>"] = cmp.mapping(
 			cmp.mapping.confirm({
 				behavior = cmp.ConfirmBehavior.Insert,
 				select = true,
