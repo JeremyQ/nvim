7c4c40504d8f0ddd0a68f8029063f657f3c33be8
Jeremy Quesada <jquesada@turo.com>
2024-06-18 10:03:53 -0700

    feat: fixing missing lua snippets and updating formatting

--- init.lua
+++ init.lua
@@ -6,17 +6,17 @@ require("options")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 -- Add Lazy to runtime path
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({import = "plugins"}, {})
+require("lazy").setup({ import = "plugins" }, {})
