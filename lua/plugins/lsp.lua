60d68f8d521542eba62e7fe25d3ac0cb7909e288
Jeremy Quesada <jquesada@turo.com>
2024-08-05 20:01:43 -0700

    feat: adding lsp hotkeys to override default

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -41,5 +41,18 @@ return {
 				require("lspconfig")[server_name].setup({})
 			end,
 		})
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function()
+				local builtin = require "telescope.builtin"
+				vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+				vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+				vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+				vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+				vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+				vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+			end
+		})
 	end,
 }
