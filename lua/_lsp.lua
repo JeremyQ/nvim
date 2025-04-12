86600e6caeee10faaae124067143baa56e2199e1
JeremyQ <jeremyq95@gmail.com>
2025-04-11 17:04:09 -0700

    feat: updating treesitter to add go and lazy lock

--- lua/_lsp.lua
+++ lua/_lsp.lua
@@ -1,11 +1,11 @@
 local on_attach = function(_, bufnr)
 	local nmap = function(keys, func, desc)
 		if desc then
-			desc = 'LSP: ' .. desc
+			desc = "LSP: " .. desc
 		end
-		vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
 	end
-	local builtin = require "telescope.builtin"
+	local builtin = require("telescope.builtin")
 
 	nmap("K", vim.lsp.buf.hover, "Hover")
 	nmap("gT", vim.lsp.buf.type_definition, "Goto Type")
@@ -19,48 +19,49 @@ local on_attach = function(_, bufnr)
 end
 
 local servers = {
+	gopls = {},
 	pyright = {
 		autoImportCompletion = true,
 		python = {
 			analysis = {
 				autoSearchPaths = true,
-				diagnosticMode = 'openFilesOnly',
+				diagnosticMode = "openFilesOnly",
 				useLibraryCodeForTypes = true,
-				typeCheckingMode = 'off'
-			}
+				typeCheckingMode = "off",
+			},
 		},
 	},
 	lua_ls = {
 		Lua = {
-			diagnostics = { disable = { 'missing-fields' } },
-		}
-	}
+			diagnostics = { disable = { "missing-fields" } },
+		},
+	},
 }
 
 -- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-mason_lspconfig.setup {
+local mason_lspconfig = require("mason-lspconfig")
+mason_lspconfig.setup({
 	ensure_installed = vim.tbl_keys(servers),
-}
+})
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- allow lsp to autocomplete lua values
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function()
 		vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-	end
+	end,
 })
 
-mason_lspconfig.setup_handlers {
+mason_lspconfig.setup_handlers({
 	function(server_name)
-		require('lspconfig')[server_name].setup {
+		require("lspconfig")[server_name].setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
 			settings = servers[server_name],
 			filetypes = (servers[server_name] or {}).filetypes,
-		}
+		})
 	end,
-}
+})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,13 +5,13 @@ return {
 			local configs = require("nvim-treesitter.configs")
 			---@diagnostic disable-next-line: missing-fields
 			configs.setup({
-				ensure_installed = { "c", "lua", "python", "vimdoc", "vim", "bash", "query" },
+				ensure_installed = { "c", "lua", "python", "go", "vimdoc", "vim", "bash", "query" },
 				sync_install = false,
 				auto_install = true,
 				ignore_install = {},
-				highlight = { enable = true, additional_vim_regex_highlighting = true, },
-				diagnostics = { disable = { 'missing-fields' } },
+				highlight = { enable = true, additional_vim_regex_highlighting = true },
+				diagnostics = { disable = { "missing-fields" } },
 			})
-		end
+		end,
 	},
 }
